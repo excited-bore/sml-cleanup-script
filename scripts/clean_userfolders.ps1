c4e940443065de45557d1ba6e4d2917ca396b59d
excited-bore <serve-churn-spiral@duck.com>
2026-02-27 03:11:08 +0000

    Fix for clean_userfolders.ps1

--- scripts/clean_userfolders.ps1
+++ scripts/clean_userfolders.ps1
@@ -1,29 +1,38 @@
+Write-Host "Cleaning up 3D Objects" -ForegroundColor Cyan
+Remove-Item -Path "C:\Users\$env:USERNAME\3D Objects\*" -Recurse -Force -ErrorAction SilentlyContinue
+
 Write-Host "Cleaning up Contacts" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Contacts\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Contacts\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Desktop" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Desktop\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Desktop\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Downloads" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Downloads\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Downloads\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Documents" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Documents\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Documents\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+Write-Host "Cleaning up Favourites" -ForegroundColor Cyan
+Remove-Item -Path "C:\Users\$env:USERNAME\Favourites\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+Write-Host "Cleaning up Links" -ForegroundColor Cyan 
+Remove-Item -Path "C:\Users\$env:USERNAME\Links\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Onedrive" -ForegroundColor Cyan 
-Remove-Item -Path "C:\User\$env:USERNAME\Onedrive\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Onedrive\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Pictures" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Pictures\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Pictures\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Music" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Music\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Music\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Saved Games" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Saved Games\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Saved Games\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Searches" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Searches\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Searches\*" -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Cleaning up Videos" -ForegroundColor Cyan
-Remove-Item -Path "C:\User\$env:USERNAME\Videos\" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Users\$env:USERNAME\Videos\*" -Recurse -Force -ErrorAction SilentlyContinue
