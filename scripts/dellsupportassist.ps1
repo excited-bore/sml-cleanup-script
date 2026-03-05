9f2237f7f64cf783e4ffc56cabd45d45093f0ec5
excited-bore <serve-churn-spiral@duck.com>
2026-03-05 18:30:39 +0100

    Added checks on whether support assist/dell commandline updater are already installed or not

--- scripts/dellsupportassist.ps1
+++ scripts/dellsupportassist.ps1
@@ -1,15 +1,20 @@
 # powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\check_wget.ps1"
 
-if (-Not (Test-Path -Path "$env:TEMP\DellSupport\" )) {
-    mkdir "$env:TEMP\DellSupport\"
-}
+if ( -not ( Test-Path -Path "C:\Program Files\Dell\SupportAssistAgent\bin\SupportAssistAgent.exe")){
+  
+    Write-Host "Installing Dell SupportAssist..." 
+    
+    if (-Not (Test-Path -Path "$env:TEMP\DellSupport\" )) {
+        mkdir "$env:TEMP\DellSupport\"
+    }
 
-if (-Not (Test-Path -Path "$env:TEMP\DellSupport\SupportAssistinstaller.exe" )){
-    Invoke-WebRequest -Method Get https://downloads.dell.com/serviceability/catalog/SupportAssistinstaller.exe -OutFile "$env:TEMP\DellSupport\SupportAssistinstaller.exe"
-}
+    if (-Not (Test-Path -Path "$env:TEMP\DellSupport\SupportAssistinstaller.exe" )){
+        Invoke-WebRequest -Method Get https://downloads.dell.com/serviceability/catalog/SupportAssistinstaller.exe -OutFile "$env:TEMP\DellSupport\SupportAssistinstaller.exe"
+    }
 
-if (Test-Path -Path $env:TEMP\DellSupport\SupportAssistinstaller.exe) {
-    Start-Process "$env:TEMP\DellSupport\SupportAssistinstaller.exe" -Wait
+    if (Test-Path -Path $env:TEMP\DellSupport\SupportAssistinstaller.exe) {
+        Start-Process "$env:TEMP\DellSupport\SupportAssistinstaller.exe" -Wait
+    }
 }
 
 # if (Test-Path variable:INS_WGET) {
--- scripts/dellupdate.ps1
+++ scripts/dellupdate.ps1
@@ -1,7 +1,14 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\check_winget.ps1"
-
-if ( -Not (Test-Path -Path "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe")){
+if (( -Not (Test-Path -Path "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe")) -and ( -Not (Test-Path -Path "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe"))) {
+    
+    Write-Host "Installing Dell Commandline." 
+  
     winget install --id Dell.CommandUpdate --accept-package-agreements --accept-source-agreements
 }
-Start-Process "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/scan" -Wait
-Start-Process "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/applyupdates" -Wait
+
+if ( Test-Path -Path "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe"){
+    Start-Process "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/scan" -Wait
+    Start-Process "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/applyupdates" -Wait
+}elseif ( Test-Path -Path "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"){
+    Start-Process "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/scan" -Wait
+    Start-Process "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" -ArgumentList "/applyupdates" -Wait
+}
