42312a12f9550157c42534ae9c4370a8d4b7a14d
excited-bore <serve-churn-spiral@duck.com>
2026-03-09 09:57:15 +0100

    Replaced every instance of Remove-Item with cmd.exe /c rd /s /q

--- scripts/clean_userfolders.ps1
+++ scripts/clean_userfolders.ps1
@@ -1,6 +1,8 @@
 Write-Host "Removing unknown files and folders from 'C:\'" -ForegroundColor Yellow
 $excludes = @('PerfLogs', 'Program Files', 'Program Files (x86)', 'Users', 'Windows')
-Get-ChildItem -Path 'C:\' | Where-Object { $_.Name -notin $excludes } | Select -ExpandProperty FullName | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path 'C:\' | Where-Object { $_.Name -notin $excludes } | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Removing unknown users from 'C:\Users'" -ForegroundColor Yellow
 $excludes = @('LaptopSML', 'Public', 'leerlingdbs')
@@ -9,43 +11,69 @@ Get-ChildItem -Path 'C:\Users\' | Where-Object { $_.Name -notin $excludes } | Fo
 }
 
 Write-Host "Cleaning up 3D Objects" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\3D Objects\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\3D Objects\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Contacts" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Contacts\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Contacts\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Desktop" -ForegroundColor Cyan
-Get-ChildItem "C:\Users\$env:USERNAME\Desktop\" -Exclude 'Google Chrome.lnk', 'PDF-Xchange Editor.lnk', 'Smartschool.lnk' | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
-#Remove-Item -Path "C:\Users\$env:USERNAME\Desktop\*" -Recurse -Force -ErrorAction SilentlyContinue
+$excludes = @('Google Chrome.lnk', 'PDF-Xchange Editor.lnk', 'Smartschool.lnk')
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Desktop\" | Where-Object { $_.Name -notin $excludes } | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Downloads" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Downloads\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Downloads\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Documents" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Documents\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Documents\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Favourites" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Favourites\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Favourites\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Links" -ForegroundColor Cyan 
-Remove-Item -Path "C:\Users\$env:USERNAME\Links\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Links\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Onedrive" -ForegroundColor Cyan 
-Remove-Item -Path "C:\Users\$env:USERNAME\Onedrive\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Onedrive\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Pictures" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Pictures\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Pictures\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Music" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Music\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Music\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Saved Games" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Saved Games\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Saved Games\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Searches" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Searches\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Searches\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 Write-Host "Cleaning up Videos" -ForegroundColor Cyan
-Remove-Item -Path "C:\Users\$env:USERNAME\Videos\*" -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Users\$env:USERNAME\Videos\" | ForEach-Object {
+    cmd.exe /c rd /s /q $_.FullName 
+}
 
 cmd.exe /c 'pause'
