58cd4757f16f5de6455745029ee64befe1096968
excited-bore <serve-churn-spiral@duck.com>
2026-03-13 01:16:23 +0100

    Fixed clean_edge and clean_chrome

--- scripts/clean_chrome.ps1
+++ scripts/clean_chrome.ps1
@@ -1,25 +1,26 @@
-$chromePath = "$env:APPDATA\Google\Chrome"
-$chromePath1 = "$env:LOCALAPPDATA\Google\Chrome"                                                                                                                                                                                                                    
+$chromeRoaming = "$env:APPDATA\Google\Chrome"
+$chromeLocal = "$env:LOCALAPPDATA\Google\Chrome\User Data"                                                                                                                                                                                                                    
 
 Write-Host "Closing Chrome if still running..." -ForegroundColor Yellow                                                                               
 Get-Process chrome -ErrorAction SilentlyContinue | Stop-Process -Force
 
-if (Test-Path $chromePath -ErrorAction SilentlyContinue) { 
-        
-	Write-Host "Deleting Chrome roaming data folder..." -ForegroundColor Cyan 
-	try {                                                                                                                                     
-		Remove-Item -Path $chromePath -Recurse -Force -ErrorAction SilentlyContinue                                                         
-	} catch {
-		Remove-Item -Path $chromePath -Recurse -Force
-	}
-}
-if (Test-Path $chromePath1 -ErrorAction SilentlyContinue){
+#if (Test-Path $chromeRoaming -ErrorAction SilentlyContinue) { 
+#        
+#	Write-Host "Deleting Chrome roaming data folder..." -ForegroundColor Cyan 
+#	try {                                                                                                                                     
+#		Remove-Item -Path $chromeRoaming -Recurse -Force -ErrorAction SilentlyContinue                                                         
+#	} catch {
+#		Remove-Item -Path $chromeRoaming -Recurse -Force
+#	}
+#}
+
+if (Test-Path $chromeLocal -ErrorAction SilentlyContinue){
 	
 	Write-Host "Deleting Chrome local data folder..." -ForegroundColor Cyan
 	try {                                                                                                                                     
-		Remove-Item -Path $chromePath1 -Recurse -Force -ErrorAction SilentlyContinue                                                         
+		Remove-Item -Path $chromeLocal -Recurse -Force -ErrorAction SilentlyContinue                                                         
 	} catch {
-		Remove-Item -Path $chromePath1 -Recurse -Force
+		Remove-Item -Path $chromeLocal -Recurse -Force
 	}
 }
 Write-Host "Chrome data has been wiped" -ForegroundColor Green 
--- scripts/clean_edge.ps1
+++ scripts/clean_edge.ps1
@@ -1,25 +1,81 @@
-$EdgePath = "$env:APPDATA\Microsoft\Edge"
-$EdgePath1 = "$env:LOCALAPPDATA\Microsoft\Edge"                                                                                                                                                                                                                    
+#$EdgeRoaming = "$env:APPDATA\Microsoft\Edge\User Data\Default"
+$EdgeLocalCookies = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Cookies"
+$EdgeLocalHistory = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\History"             
+$EdgeLocalWebdata = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Web Data"
+$EdgeLocalBookmarks = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Bookmark*"
+$EdgeLocalExtensions = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Extensions\*"
 
 Write-Host "Closing Edge if still running..." -ForegroundColor Yellow                                                                               
 Get-Process msedge -ErrorAction SilentlyContinue | Stop-Process -Force
 
-if (Test-Path $EdgePath -ErrorAction SilentlyContinue) { 
-        
-	Write-Host "Deleting Edge roaming data folder..." -ForegroundColor Cyan
-	try {                                                                                                                                     
-		Remove-Item -Path $EdgePath -Recurse -Force -ErrorAction SilentlyContinue                                                         
-	} catch {
-		Remove-Item -Path $EdgePath -Recurse -Force
-	}
-}
-if (Test-Path $EdgePath1 -ErrorAction SilentlyContinue){
+#if (Test-Path $EdgeRoaming -ErrorAction SilentlyContinue) { 
+#        
+#	Write-Host "Deleting Edge roaming data folder..." -ForegroundColor Cyan
+#	try {                                                                                                                                     
+#		Remove-Item -Path $EdgeRoaming -Recurse -Force -ErrorAction SilentlyContinue                                                         
+#	} catch {
+#		Remove-Item -Path $EdgeRoaming -Recurse -Force
+#	}
+#}
+
+# Cookies
+
+if (Test-Path $EdgeLocalCookies -ErrorAction SilentlyContinue){
 	
-	Write-Host "Deleting Edge local data folder..." -ForegroundColor Cyan
+	Write-Host "Deleting Edge local cookies data folder..." -ForegroundColor Cyan
 	try {                                                                                                                                     
-		Remove-Item -Path $EdgePath1 -Recurse -Force -ErrorAction SilentlyContinue                                                         
+		Remove-Item -Path $EdgeLocalCookies -Recurse -Force -ErrorAction SilentlyContinue                                                         
 	} catch {
-		Remove-Item -Path $EdgePath1 -Recurse -Force
+		Remove-Item -Path $EdgeLocalCookies -Recurse -Force
 	}	
 }
+
+# History
+
+if (Test-Path $EdgeLocalHistory -ErrorAction SilentlyContinue){
+	
+	Write-Host "Deleting Edge local history data folder..." -ForegroundColor Cyan
+	try {                                                                                                                                     
+		Remove-Item -Path $EdgeLocalHistory -Recurse -Force -ErrorAction SilentlyContinue                                                         
+	} catch {
+		Remove-Item -Path $EdgeLocalHistory -Recurse -Force
+	}	
+}
+
+# Web data
+
+if (Test-Path $EdgeLocalWebdata -ErrorAction SilentlyContinue){
+	
+	Write-Host "Deleting Edge local web data folder..." -ForegroundColor Cyan
+	try {                                                                                                                                     
+		Remove-Item -Path $EdgeLocalWebdata -Recurse -Force -ErrorAction SilentlyContinue                                                         
+	} catch {
+		Remove-Item -Path $EdgeLocalWebdata -Recurse -Force
+	}	
+}
+
+# Bookmarks 
+
+if (Test-Path $EdgeLocalBookmarks -ErrorAction SilentlyContinue){
+	
+	Write-Host "Deleting Edge local bookmarks..." -ForegroundColor Cyan
+	try {                                                                                                                                     
+		Remove-Item -Path $EdgeLocalBookmarks -Recurse -Force -ErrorAction SilentlyContinue                                                         
+	} catch {
+		Remove-Item -Path $EdgeLocalBookmarks -Recurse -Force
+	}	
+}
+
+# Extensions 
+
+if (Test-Path $EdgeLocalExtensions -ErrorAction SilentlyContinue){
+	
+	Write-Host "Deleting Edge local extensions..." -ForegroundColor Cyan
+	try {                                                                                                                                     
+		Remove-Item -Path $EdgeLocalExtensions -Recurse -Force -ErrorAction SilentlyContinue                                                         
+	} catch {
+		Remove-Item -Path $EdgeLocalExtensions -Recurse -Force
+	}	
+}
+
 Write-Host "Edge data has been wiped" -ForegroundColor Green 
