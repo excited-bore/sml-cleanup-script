a8ec86f19838f822ddbe693142205cead622b1ce
excited-bore <serve-churn-spiral@duck.com>
2026-03-13 07:23:07 +0100

    Added to remove packages: all of the unnecessary .appx packages from msstore which autoremove now

--- scripts/remove_packages.ps1
+++ scripts/remove_packages.ps1
@@ -7,6 +7,9 @@ if ( -not ( Test-Path -path "C:\Program Files\WindowsPowerShell\Modules\Microsof
 	Install-Module -Force Microsoft.Winget.Client
 }
 
+Write-Host "Attempting to update firmware before removing programs and locking down system"
+
+
 if ( $manufacturer -eq 'Dell Inc.'){
     
     Write-Host "Dell System detected. Installing Dell Commandline"
@@ -49,6 +52,83 @@ if ( $manufacturer -eq 'Dell Inc.'){
     $filePath = "$PSScriptRoot\packages-dbs\packages-test.csv"
 
 }
+
+Write-Host "Removing some Microsoft store apps"
+Write-Host "Removing Teams"
+Get-AppxPackage -AllUsers MSTeams | Remove-AppxPackage -AllUsers
+# Prevent it being reinstalled for new users
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'MSTeams' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Outlook"
+Get-AppxPackage -AllUsers Microsoft.OutlookForWindows | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.OutlookForWindows' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Sticky Notes"
+Get-AppxPackage -AllUsers Microsoft.MicrosoftStickyNotes | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.MicrosoftStickyNotes' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Microsoft DevHome"
+Get-AppxPackage -AllUsers Microsoft.Windows.DevHome | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.Windows.DevHome' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Microsoft Widgets"
+Get-AppxPackage -AllUsers MicrosoftWindows.Client.WebExperience | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.WidgetsPlatformRuntime | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'MicrosoftWindows.Client.WebExperience' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.WidgetsPlatformRuntime' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Microsoft StartExperiencesApp"
+Get-AppxPackage -AllUsers Microsoft.StartExperiencesApp | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.StartExperiencesApp' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Linkedin (if it's installed)"
+Get-AppxPackage -AllUsers 7EE7776C.LinkedInforWindows | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like '7EE7776C.LinkedInforWindows' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing cross device related programs (Crossdevice, quickassist, yourphone)"
+Get-AppxPackage -AllUsers MicrosoftWindows.CrossDevice | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers MicrosoftCorporationII.QuickAssist | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'MicrosoftWindows.CrossDevice' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'MicrosoftCorporationII.QuickAssistMicrosoft.YourPhone' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.OutlookForWindows' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Todos"
+Get-AppxPackage -AllUsers Microsoft.Todos | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.Todos' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Feedback hub"
+Get-AppxPackage -AllUsers Microsoft.WindowsFeedbackHub | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.WindowsFeedbackHub' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Bing related programs, like bing news, search and weather"
+Get-AppxPackage -AllUsers Microsoft.BingNews | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.BingSearch | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.BingWeather | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.BingNews' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.BingSearch' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.BingWeather' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Solitaire Collection"
+Get-AppxPackage -AllUsers Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.MicrosoftSolitaireCollection' | Remove-AppxProvisionedPackage -Online
+
+Write-Host "Removing Xbox and other game related programs"
+Get-AppxPackage -AllUsers Microsoft.GamingApp | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.Edge.GameAssist | Remove-AppxPackage -AllUsers
+# Get-AppxPackage -AllUsers Microsoft.XboxGameCallableUI | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.XboxIdentityProvider | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers Microsoft.Xbox.TCUI | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.GamingApp' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.Edge.GameAssist' | Remove-AppxProvisionedPackage -Online
+# Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.XboxGameCallableUI' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.XboxIdentityProvider' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.XboxSpeechToTextOverlay' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.XboxGamingOverlay' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like 'Microsoft.Xbox.TCUI' | Remove-AppxProvisionedPackage -Online
+
 
 $packages = @(Get-WinGetPackage | Select-Object Name, Id)
 if ( Test-Path -path $filePath){
